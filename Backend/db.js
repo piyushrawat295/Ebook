5d3ba4877db2a9e9e0b457094b260db14c38b0cd
Piyush Rawat <unexpected086@gmail.com>
2024-02-26 22:12:22 +0530

    Third Day

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true";
+// const mongoURI = "mongodb+srv://Piyush295:20@07@2005Op@cluster0.3qfibbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongoURI = "mongodb://127.0.0.1:27017/ebook?directConnection=true";
 
 const connectToMongo = async () => {
