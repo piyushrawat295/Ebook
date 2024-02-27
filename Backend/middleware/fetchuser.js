3b8f28729f8f6af5e9b90beb4540adb9f853fb5e
Piyush Rawat <unexpected086@gmail.com>
2024-02-28 00:04:34 +0530

    Refactor

--- Backend/middleware/fetchuser.js
+++ Backend/middleware/fetchuser.js
@@ -1,18 +1,18 @@
 var jwt = require('jsonwebtoken');
 
-const JWT_SECRET = '@pIYUwEB';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const fetchuser = (req, res, next) => {
     const token = req.header('auth-token');
-    if(!token){
-        res.status(401).send({error: "Please authenticate using valid token"})
+    if (!token) {
+        res.status(401).send({error: "Please authenticate using a valid token"});
     }
-    try{
-        const data =jwt.verify(token, JWT_SECRET);
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
         req.user = data.user;
         next();
-    }catch(error){
-        res.status(401).send({error: "Please authenticate using valid token"})
+    } catch (error) {
+        res.status(401).send({error: "Please authenticate using a valid token"});
     }
 }
 
--- Backend/routes/auths.js
+++ Backend/routes/auths.js
@@ -3,11 +3,10 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-var jwt = require('jsonwebtoken');
-var fetchuser = require('../middleware/fetchuser');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 
-
-const JWT_SECRET = "@pIYUwEB";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/newuser', [
     body('name').isLength({ min: 3 }),
@@ -17,7 +16,7 @@ router.post('/newuser', [
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ success,errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     try {
         let user = await User.findOne({ email: req.body.email });
@@ -38,8 +37,8 @@ router.post('/newuser', [
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
 
-        success=true;
-        res.json({ success,authtoken });
+        success = true;
+        res.json({ success, authtoken });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some Error occurred");
@@ -60,7 +59,7 @@ router.post('/login', [
         let user = await User.findOne({ email });
         if (!user) {
             success = false;
-            return res.status(400).json({success, error: "Please try to login with correct credentials" });
+            return res.status(400).json({ success, error: "Please try to login with correct credentials" });
         }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
@@ -74,22 +73,22 @@ router.post('/login', [
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
         success = true;
-        res.json({ success,authtoken });
+        res.json({ success, authtoken });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some Error occurred");
     }
 });
 
-router.post('/getuser', fetchuser, async (req, res) =>{
-    try{
-        userID = req.user.id;
-        const user = await User.findById(userID).select("-password")
-        res.send(user)
-    }catch(error){
+router.post('/getuser', fetchuser, async (req, res) => {
+    try {
+        const userID = req.user.id;
+        const user = await User.findById(userID).select("-password");
+        res.send(user);
+    } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
-})
+});
 
 module.exports = router;
